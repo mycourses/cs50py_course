2ea64eb98817cb80ed0ab7a7dc6e7214f7716fbf
mycourses <mycourses@users.noreply.github.com>
2022-04-24 22:12:34 +0200

    Add playback.py

---
+++ ps0/playback.py
@@ -0,0 +1,10 @@
+# CS50 Python 2022
+# Problem Set 0 - Playback Speed
+# v1.0
+
+
+user_prompt = "Slow down sunshine - can hardly follow you:  "
+
+response = input(user_prompt)
+print(response.replace(" ", "..."))
+
