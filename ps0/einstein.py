3ff2ce8f4bc3f97256459c060aa07bc82c7beaeb
mycourses <mycourses@users.noreply.github.com>
2022-04-24 22:37:11 +0200

    Add einstein.py

---
+++ ps0/einstein.py
@@ -0,0 +1,10 @@
+# CS50 Python 2022
+# Problem Set 0 - Einstein
+# v1.0
+
+
+user_prompt = "Enter Mass in Kg as an Integer: "
+
+response = int(input(user_prompt))
+
+print("E:", response * 300000000 ** 2)
